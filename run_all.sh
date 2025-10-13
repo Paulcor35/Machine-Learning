feaae1b0a3240a4144b84a0968a7cb928573f48e
GUERMEUR-Nicolas <ng.stcharles@gmail.com>
2025-10-13 15:56:14 +0200

    Variables are good

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,11 @@
-./main.py -f data/ozone_complet.txt -s $1 -t r -F maxO3 -a DecisionTree
-./main.py -f data/Carseats.csv -s $1 -t c -F High -a DecisionTree
-./main.py -f data/ozone_complet.txt -s $1 -t r -F maxO3 -a RandomForest
-./main.py -f data/Carseats.csv -s $1 -t c -F High -a RandomForest
-./main.py -f data/ozone_complet.txt -s $1 -t r -F maxO3 -a Ridge
-./main.py -f data/ozone_complet.txt -s $1 -t r -F maxO3 -a Lasso
-./main.py -f data/ozone_complet.txt -s $1 -t r -F maxO3 -a SVM
-./main.py -f data/Carseats.csv -s $1 -t c -F High -a SVM
+reg='data/ozone_complet.txt'
+cls='data/Carseats.csv'
+
+./main.py -f "$reg" -s $1 -t r -F maxO3 -a DecisionTree
+./main.py -f "$cls" -s $1 -t c -F High -a DecisionTree
+./main.py -f "$reg" -s $1 -t r -F maxO3 -a RandomForest
+./main.py -f "$cls" -s $1 -t c -F High -a RandomForest
+./main.py -f "$reg" -s $1 -t r -F maxO3 -a Ridge
+./main.py -f "$reg" -s $1 -t r -F maxO3 -a Lasso
+./main.py -f "$reg" -s $1 -t r -F maxO3 -a SVM
+./main.py -f "$cls" -s $1 -t c -F High -a SVM
