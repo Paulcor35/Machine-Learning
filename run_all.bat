2f958b7756dd255dd26d97edf914e582e6ea7738
William <willedg.collet@gmail.com>
2025-10-13 15:32:50 +0200

    Suppression dataset_prepared + changement du nom de dossier en data

--- run_all.bat
+++ run_all.bat
@@ -3,13 +3,13 @@ REM Usage : run_all.bat all
 
 set ARG=%1
 
-py main.py -f Data-20251001/ozone_complet.txt -s %ARG% -t r -F maxO3 -a DecisionTree
-py main.py -f Data-20251001/Carseats_prepared.csv -s %ARG% -t c -F High -a DecisionTree
-py main.py -f Data-20251001/ozone_complet.txt -s %ARG% -t r -F maxO3 -a RandomForest
-py main.py -f Data-20251001/Carseats_prepared.csv -s %ARG% -t c -F High -a RandomForest
-py main.py -f Data-20251001/ozone_complet.txt -s %ARG% -t r -F maxO3 -a Ridge
-py main.py -f Data-20251001/ozone_complet.txt -s %ARG% -t r -F maxO3 -a Lasso
-py main.py -f Data-20251001/ozone_complet.txt -s %ARG% -t r -F maxO3 -a SVM
-py main.py -f Data-20251001/Carseats_prepared.csv -s %ARG% -t c -F High -a SVM
+py main.py -f data/ozone_complet.txt -s %ARG% -t r -F maxO3 -a DecisionTree
+py main.py -f data/Carseats_prepared.csv -s %ARG% -t c -F High -a DecisionTree
+py main.py -f data/ozone_complet.txt -s %ARG% -t r -F maxO3 -a RandomForest
+py main.py -f data/Carseats_prepared.csv -s %ARG% -t c -F High -a RandomForest
+py main.py -f data/ozone_complet.txt -s %ARG% -t r -F maxO3 -a Ridge
+py main.py -f data/ozone_complet.txt -s %ARG% -t r -F maxO3 -a Lasso
+py main.py -f data/ozone_complet.txt -s %ARG% -t r -F maxO3 -a SVM
+py main.py -f data/Carseats_prepared.csv -s %ARG% -t c -F High -a SVM
 
 pause
